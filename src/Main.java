ce48d335be04556173c628ceab06295a8b954fdb
Leonishe <beloozersk@hotmail.com>
2013-10-08 11:30:40 +0400

    First commit

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,30 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.*;
 
+import java.io.File;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 public class Main {
 
-    public static void main(String[] args) {
 
-    }
+
+
+        public static void main(String[] args) {
+
+            try {
+
+                File file = new File("SoftList.xml");
+                JAXBContext jaxbContext = JAXBContext.newInstance(Soft.class);
+
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                Soft customer = (Soft) jaxbUnmarshaller.unmarshal(file);
+                System.out.println(customer);
+
+            } catch (JAXBException e) {
+                e.printStackTrace();
+            }
+
+        }
+
 }
